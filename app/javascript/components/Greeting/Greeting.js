33bd5e895a274f2033d14d3387942a93676642df
skyv26 <dev.aakashv@gmail.com>
2023-01-22 22:52:01 +0530

    Add `greeting` component

--- app/javascript/components/Greeting/Greeting.js
+++ app/javascript/components/Greeting/Greeting.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import style from './Greeting.module.css';
 
 const Greeting = () => {
   const message = useSelector((state) => state.greeting);
   const { greeting } = message;
   return (
-    <h1>{greeting}</h1>
+    <div className={style.Greeting}>
+      <h1>{greeting}</h1>
+    </div>
   );
 };
 
