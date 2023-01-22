0c92c1ac92d894fc244611bb344db66d5a3f842a
skyv26 <dev.aakashv@gmail.com>
2023-01-22 21:11:59 +0530

    Remove the `console`

--- app/javascript/App.js
+++ app/javascript/App.js
@@ -1,5 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { Routes, Route, Navigate } from 'react-router';
+import { GreetingThunk } from './redux/greetingSlice';
+import Greeting from './components/Greeting/Greeting';
 
-const App = () => <h1>Hell World !</h1>;
+const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(GreetingThunk());
+  }, [dispatch]);
+  return (
+    <Routes>
+      <Route path="/greetings" element={<Greeting />} />
+      <Route path="/about" element={<h1>Hello World!</h1>} />
+      <Route path="/" element={<Navigate replace to="/greetings" />} />
+    </Routes>
+  );
+};
 
 export default App;
