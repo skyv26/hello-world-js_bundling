d10a6a9b4c43f9bfc8ecd2f3ae505fb47607392f
skyv26 <dev.aakashv@gmail.com>
2023-01-22 22:54:30 +0530

    Add `styles`

--- app/javascript/App.js
+++ app/javascript/App.js
@@ -1,8 +1,11 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { Routes, Route, Navigate } from 'react-router';
+import Header from './components/Header/Header';
 import { GreetingThunk } from './redux/greetingSlice';
 import Greeting from './components/Greeting/Greeting';
+import About from './components/About/About';
+import './App.css';
 
 const App = () => {
   const dispatch = useDispatch();
@@ -10,11 +13,14 @@ const App = () => {
     dispatch(GreetingThunk());
   }, [dispatch]);
   return (
-    <Routes>
-      <Route path="/greetings" element={<Greeting />} />
-      <Route path="/about" element={<h1>Hello World!</h1>} />
-      <Route path="/" element={<Navigate replace to="/greetings" />} />
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/greetings" element={<Greeting />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/" element={<Navigate replace to="/greetings" />} />
+      </Routes>
+    </>
   );
 };
 
