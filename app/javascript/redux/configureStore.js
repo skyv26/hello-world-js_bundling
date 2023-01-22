f3f708c4e2469bd1028d23901b93fe5cd950bef1
skyv26 <dev.aakashv@gmail.com>
2023-01-22 20:27:41 +0530

    Create `configureStore` file

---
+++ app/javascript/redux/configureStore.js
@@ -0,0 +1,12 @@
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import greetingSlice from './greetingSlice';
+
+const reducer = combineReducers({
+  greeting: greetingSlice,
+});
+
+const store = configureStore({
+  reducer,
+});
+
+export default store;
