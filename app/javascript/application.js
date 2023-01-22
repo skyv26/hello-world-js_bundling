e26e0b49817032b5870fd56c82243fcfe3e00e04
skyv26 <dev.aakashv@gmail.com>
2023-01-22 20:56:26 +0530

    Resolve the `export` issue

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,11 +5,15 @@ import './controllers';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/configureStore';
 import App from './App';
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>,
   document.getElementById('root'),
 );
