3bd0888656991a535c554e37d3449485aff8af95
skyv26 <dev.aakashv@gmail.com>
2023-01-22 18:58:09 +0530

    Add `api/v1` routes for `greetings`

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      get 'greetings#index'
+      resources :greetings
     end
   end
   # Defines the root path route ("/")
