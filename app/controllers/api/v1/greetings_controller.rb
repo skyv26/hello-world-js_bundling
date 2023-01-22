2a90c1351b13d80d7ffa4a4e140fe007dd55aaed
skyv26 <dev.aakashv@gmail.com>
2023-01-22 16:43:13 +0530

    update the `greetings#index` action

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -2,6 +2,8 @@ module Api
   module V1
     class GreetingsController < ApplicationController
       def index
+        greetings = Message.all
+        render json: greetings
       end
     end
   end
